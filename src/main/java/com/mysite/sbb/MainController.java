7189b072d7386516d67a44e2641cc7d9f96d6753
daydreaam <dayingyeo@naver.com>
2024-12-10 10:32:20 +0900

    first Mapping controller

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public void home(){
+        System.out.println("Home page request");
+    }
+    @GetMapping("/about")
+    public void about(){
+        System.out.println("about request");
+    }
+}
